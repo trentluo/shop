0f39cf795d1a21e0f0369d04a9636a8cb989bcaf
rencaiyu <rencaiyu@sh-rencaiyu.netfinworks.com>
2015-05-19 18:39:36 +0800

    init

--- src/main/java/com/rency/shop/web/action/ErrorAction.java
+++ src/main/java/com/rency/shop/web/action/ErrorAction.java
@@ -1,25 +1,14 @@
 package com.rency.shop.web.action;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.jasig.cas.client.util.CommonUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import com.netfinworks.service.exception.ServiceException;
-import com.netfinworks.site.core.common.RestResponse;
-import com.netfinworks.site.core.common.constants.CommonConstant;
-import com.netfinworks.site.domain.domain.member.EnterpriseMember;
-import com.netfinworks.site.domain.enums.ResourceInfo;
-import com.netfinworks.site.web.action.common.BaseAction;
 
 /**
  * <p>
@@ -42,15 +31,7 @@ public class ErrorAction extends BaseAction {
 	@RequestMapping(value = "/error.htm", method = { RequestMethod.POST,RequestMethod.GET })
 	public ModelAndView error(HttpServletRequest req, HttpServletResponse resp,ModelMap model) {
 		HttpSession session = req.getSession();
-		String msg = (String) session.getAttribute(CommonConstant.ERROR);
-		RestResponse restP = new RestResponse();
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("member", user);
-		restP.setData(data);
-		model.put("response", user);
-		model.put("error", msg);
-		restP.setLogoutUrl(CommonUtils.getUcLogoutUrl(req, resp, ""));
-		return new ModelAndView(ResourceInfo.ERROR.getUrl(), "response", restP);
+		return new ModelAndView("url", "response", "obj");
 	}
 
 	/**
@@ -60,22 +41,10 @@ public class ErrorAction extends BaseAction {
 	 * @param request
 	 * @return
 	 */
-	@RequestMapping(value = "/error/404.htm", method = { RequestMethod.POST,
-			RequestMethod.GET })
-	public ModelAndView error404(HttpServletRequest req,
-			HttpServletResponse resp, ModelMap model) {
+	@RequestMapping(value = "/error/404.htm", method = { RequestMethod.POST,RequestMethod.GET })
+	public ModelAndView error404(HttpServletRequest req,HttpServletResponse resp, ModelMap model) {
 		HttpSession session = req.getSession();
-		EnterpriseMember user = (EnterpriseMember) session
-				.getAttribute(CommonConstant.SESSION_ATTR_NAME_CURRENT_USER);
-		String msg = (String) session.getAttribute(CommonConstant.ERROR);
-		RestResponse restP = new RestResponse();
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("member", user);
-		restP.setData(data);
-		model.put("response", user);
-		model.put("error", msg);
-		restP.setLogoutUrl(CommonUtils.getUcLogoutUrl(req, resp, ""));
-		return new ModelAndView("common/error/404", "response", restP);
+		return new ModelAndView("common/error/404", "response", "obj");
 	}
 
 	/**
@@ -85,70 +54,10 @@ public class ErrorAction extends BaseAction {
 	 * @param request
 	 * @return
 	 */
-	@RequestMapping(value = "/error/500.htm", method = { RequestMethod.POST,
-			RequestMethod.GET })
-	public ModelAndView error500(HttpServletRequest req,
-			HttpServletResponse resp, ModelMap model) {
+	@RequestMapping(value = "/error/500.htm", method = { RequestMethod.POST,RequestMethod.GET })
+	public ModelAndView error500(HttpServletRequest req,HttpServletResponse resp, ModelMap model) {
 		HttpSession session = req.getSession();
-		EnterpriseMember user = (EnterpriseMember) session
-				.getAttribute(CommonConstant.SESSION_ATTR_NAME_CURRENT_USER);
-		String msg = (String) session.getAttribute(CommonConstant.ERROR);
-		RestResponse restP = new RestResponse();
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("member", user);
-		restP.setData(data);
-		model.put("response", user);
-		model.put("error", msg);
-		restP.setLogoutUrl(CommonUtils.getUcLogoutUrl(req, resp, ""));
-		return new ModelAndView("common/error/500", "response", restP);
+		return new ModelAndView("common/error/500", "response", "obj");
 	}
 
-	/**
-	 * 进入公司ID查询错误页面
-	 * 
-	 * @param model
-	 * @param request
-	 * @return
-	 */
-
-	@RequestMapping(value = "/my/noCorpError.htm", method = {
-			RequestMethod.POST, RequestMethod.GET })
-	public ModelAndView noCorpError(HttpServletRequest req,
-			HttpServletResponse resp) throws ServiceException {
-		String userName = String.valueOf(req
-				.getAttribute(CommonConstant.SESSION_ATTR_NAME_CURRENT_USER));
-		req.setAttribute("message", "对不起：[" + userName + "] 不是商户，无法进入企业钱包");
-		req.setAttribute("errorType", "1");
-		RestResponse restP = new RestResponse();
-		Map<String, Object> data = new HashMap<String, Object>();
-		EnterpriseMember user = new EnterpriseMember();
-		user.setMemberName(userName);
-		data.put("member", user);
-		restP.setData(data);
-		restP.setLogoutUrl(CommonUtils.getUcLogoutUrl(req, resp, ""));
-		return new ModelAndView("/common/exception/loginError", "response",
-				restP);
-	}
-
-	/**
-	 * 进入错误页面
-	 * 
-	 * @param model
-	 * @param request
-	 * @return
-	 */
-	@RequestMapping(value = "/my/toPersonWallet.htm", method = {
-			RequestMethod.POST, RequestMethod.GET })
-	public ModelAndView toPersonWallet(HttpServletRequest req,
-			HttpServletResponse resp) {
-		HttpSession session = req.getSession();
-		EnterpriseMember user = (EnterpriseMember) session
-				.getAttribute(CommonConstant.SESSION_ATTR_NAME_CURRENT_USER);
-		RestResponse restP = new RestResponse();
-		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("member", user);
-		restP.setData(data);
-		restP.setLogoutUrl(CommonUtils.getUcLogoutUrl(req, resp, ""));
-		return new ModelAndView("toPersonWallet", "response", restP);
-	}
 }
